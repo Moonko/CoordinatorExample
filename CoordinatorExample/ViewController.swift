2e3746782c9edebc00b0ce49d3e365dd10beeeb8
Andrei Rychkov <rychkov.dev@gmail.com>
2018-05-03 22:52:07 +0300

    Create project

---
+++ CoordinatorExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoordinatorExample
+//
+//  Created by Андрей Рычков on 30/04/2018.
+//  Copyright © 2018 Andrei Rychkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
